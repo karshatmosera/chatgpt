cf4cd81c72e1094517bee3309202111b5a08d7f3
Karsh <karsh.sahay@atmosera.com>
2023-04-04 15:48:07 -0600

    Adding Streamlit Chatbot

---
+++ Streamlit-ChatBot.py
@@ -0,0 +1,46 @@
+#Import the libraries
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+openai.api_key = "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"
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+        
+    )
+
+    message = completions.choices[0].text
+    return message
+
+st.title("ChatGPT - Davinci Model")
+
+#Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text
+
+user_input = get_text() 
+
+if user_input:
+    output = generate_response(user_input)
+    # store the output
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
