03dbb7a3acece9f2814472c6600b28436d15295a
Karsh Sahay <karsh.sahay@atmosera.com>
2023-04-06 19:28:07 -0600

    Added Curie engine in commented out phase

--- Streamlit-ChatBot.py
+++ Streamlit-ChatBot.py
@@ -12,6 +12,7 @@ openai.api_base = "https://karshgpt.openai.azure.com/"
 openai.api_type = 'azure'
 openai.api_version = '2022-12-01' # this may change in the future
 
+#deployment_name ="Karsh-Curie-Test"
 deployment_name='Karsh-Ada-Test'
 
 @st.cache_data
