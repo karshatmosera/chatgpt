3e7e03ee4da0b435b5f11a1d6312501abb919298
Karsh <karsh.sahay@atmosera.com>
2023-04-17 16:42:56 -0600

    changed to Azure OpenAI secrets

--- ChatGPT.py
+++ ChatGPT.py
@@ -6,8 +6,15 @@ import toml
 secrets_file = os.path.join(os.path.dirname(__file__), 'secrets.toml')
 secrets = toml.load(secrets_file)
 
-openai_api_key = secrets['chatgpt']['api_key']
+openai_api_key = secrets['openai']['api_key']
+openai_api_base = secrets['openai']['api_base']
+openai_api_type = secrets['openai']['api_type']
+openai_api_version = secrets['openai']['api_version']
+
 openai.api_key = openai_api_key
+openai.api_base = openai_api_base
+openai.api_type = openai_api_type
+openai.api_version = openai_api_version
 
 messages = [
  {"role": "system", "content" : ""}
@@ -19,6 +26,7 @@ while True:
     
     completion = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
+      engine='gpt35',
       messages=messages
     )
 
